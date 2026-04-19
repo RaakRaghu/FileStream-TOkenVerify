80cc0db0a6151836b7f765673695079d8135f9b9
RaakRaghu <123383385+RaakRaghu@users.noreply.github.com>
2026-04-19 19:00:16 +0530

    Remove unused import for verification module

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -12,7 +12,6 @@ from pyrogram.errors import UserNotParticipant
 from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
-from Adarsh.bot.plugins import verification
 
 if MY_PASS:
             buttonz=ReplyKeyboardMarkup(
